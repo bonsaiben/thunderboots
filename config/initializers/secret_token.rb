c3c76dd0ed18d8555b6b2807fd917de90ece06d5
Benjamin Sullivan <bsullivan2@gmail.com>
2012-06-22 03:11:21 +0900

    removed everything except the relevant files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Thunderboots::Application.config.secret_token = '3b725050f6e9e3c0518a536a5a7b4f9f0d8260f43da4a5652556914890afa93828a148727f06661bedf8f829c6e002d08c8715feb34ccca7d5d55d5119b717f9'
